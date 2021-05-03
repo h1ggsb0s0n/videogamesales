729fbdba1ac147f3f17530bcac17c15e46624e96
h1ggsb0s0n <53830849+h1ggsb0s0n@users.noreply.github.com>
2021-05-03 22:56:22 +0200

    Kommentare hinzugefügt

--- src/main/java/com/vaadin/tutorial/crm/ui/VideoGameView.java
+++ src/main/java/com/vaadin/tutorial/crm/ui/VideoGameView.java
@@ -29,12 +29,12 @@ public class VideoGameView extends VerticalLayout {
         setSizeFull();
 
 
-        this.getFirst100VideoGames();
+        this.getFirst1000VideoGames();
 
         Button mapButton = new Button("Map");
         mapButton.addClickListener(clickEvent -> mapOperation());
 
-        Button filterSortLimit = new Button("filterSortLimit");
+        Button filterSortLimit = new Button("filter Sort Limit");
         filterSortLimit.addClickListener(clickEvent -> filterSortLimit());
 
         Button skipFirstTenRanksButton = new Button("Skip 10 Ranks");
@@ -70,25 +70,39 @@ public class VideoGameView extends VerticalLayout {
 
     }
 
-
-    private void getFirst100VideoGames(){
+    /**
+     * Holt die ersten 1000 Ränge an Video Spielen und shuffled sie damit sie unsortiert getestet werden können
+     */
+    private void getFirst1000VideoGames(){
         this.listOfVideoGames = this.vgService.findByRankBetween(1L, 1000L);
-        Notification.show("Number of items in List" + this.listOfVideoGames.size());;
+        Collections.shuffle(this.listOfVideoGames);
+        Notification.show("Number of items in List" + this.listOfVideoGames.size());
 
     }
 
-
+    /**
+     * Die Methode erstellt einen Video Game Namen.
+     * Enthält ein Name in Capital Letters um das vorhandensein eines Namen zu testen.
+     * Enthält einen Kurznamen zur Darstellung in einem Typeahead.
+     */
     private void mapOperation(){
         CollectionOperations.CreateVideoGameName(this.listOfVideoGames);
     }
 
-    //Filter Sort Limit
+    /**
+     * Filtert nach Jahr > 2014
+     * Sortiert nach Jahr
+     * Limitiert die ersten 20 Resultate
+     *
+     */
     private void filterSortLimit(){
         CollectionOperations.filterSortLimit(this.listOfVideoGames);
     }
 
-    //skip
 
+    /**
+     * Sortiert nach Rang und Überspringt die ersten 10 Ränge
+     */
     private void SkipFirstTenRanks(){
         System.out.println("Skip First 10 Ranks: ");
         Collections.shuffle(this.listOfVideoGames);
@@ -96,51 +110,64 @@ public class VideoGameView extends VerticalLayout {
     }
 
 
-    //anymatch
-
+    /**
+     *  Gibt True auf der Console aus wenn es ein Spiel mit dem Anfangsbuchstaben B enthält
+     */
     private void startsWithB(){
         System.out.println("Is there a Game that starts with B: " + CollectionOperations.videoGameStartsWith(this.listOfVideoGames, "B"));
     }
 
+
+    /**
+     * Negativ Test: Gibt True wenn ein Spiel mit den Anfangsbuchstaben ASDF gefunden wurde.
+     */
     private void startsWithASDF(){
         System.out.println("Is there a Game that starts with ASDF: " + CollectionOperations.videoGameStartsWith(this.listOfVideoGames, "ASDF"));
     }
 
-    //matchall
 
+    /**
+     * Tested ob alle Borderland Spiele das Genre Shooter haben.
+     * Negativ Test -> Assesins creed
+     * Gibt true oder False auf der Commandozeile aus.
+     */
     private void compareGenreOfBorderlandsGames(){
+        System.out.println("Compare Genre of Borderlands Games");
         List<VideoGame> codGames = vgService.findByNameContaining("Borderlands");
-        //System.out.println(codGames.size());
-        //codGames.forEach(vg -> System.out.println(vg.getName() + " Genre: "+ vg.getGenre()));
         System.out.println("All Genre the same: " + CollectionOperations.checkIfGenreIsTheSame(codGames, "Shooter"));
+        //negativTest
+
+        System.out.println("Compare Genre of Assassin Games");
+        List<VideoGame> assasinscreedGame = vgService.findByNameContaining("Assassin");
+        System.out.println("All Genre the same of Assasins Creed: " + CollectionOperations.checkIfGenreIsTheSame(assasinscreedGame, "Shooter"));
 
     }
 
-    //negative check with assasin's creed
-
-
+    /**
+     * Zählt die Anzahl an Borderland Spiele
+     */
     private void getCountOfBorderlandsGames(){
         List<VideoGame> borderlandsGames = vgService.findByNameContaining("Borderlands");
-        System.out.println(borderlandsGames.size());
-        borderlandsGames.forEach(vg -> System.out.println(vg.getName() + " Genre: "+ vg.getGlobal_sales()));
         System.out.println(CollectionOperations.numberOfGames(borderlandsGames));
 
     }
 
 
-
+    /**
+     * Gibt das am wenigsten Verkaufte Borderlands Game auf der Kommandozeile aus.
+     */
     private void getLowestSellingBorderlandsGame(){
         List<VideoGame> borderlandsGames = vgService.findByNameContaining("Borderlands");
-        //Da die Liste schon sortiert ist wird testmässig geshuffelt
-        Collections.shuffle(borderlandsGames);
         VideoGame lowestSellingGame = CollectionOperations.getLowestTotalSales(borderlandsGames);
         System.out.println("The lowest selling game in the Borderlands Series is: ");
         System.out.println("Game:" + lowestSellingGame.getName() + " Global Sales: " +  lowestSellingGame.getGlobal_sales());
     }
 
+    /**
+     * gibt die die 10 meistverkauften Borderlands spiele auf der Kommandozeile aus.
+     */
     private void getFirst10Ranks(){
         List<VideoGame> borderlandsGames = vgService.findByNameContaining("Borderlands");
-        Collections.shuffle(borderlandsGames);
         List<VideoGame> fiveMostPopularBorderlandsGames = CollectionOperations.get5MostPopularResults(borderlandsGames);
         fiveMostPopularBorderlandsGames.forEach(
                 vg -> System.out.println("Rank: " + vg.getRank() + " Name: " + vg.getName())
